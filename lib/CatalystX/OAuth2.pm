10ec41d1a4b009f3feec142c46872369269e679a
Gavin Henry <ghenry@suretecsystems.com>
2012-05-30 20:32:39 +0200

    Update SPONSORSHIP text.

--- lib/CatalystX/OAuth2.pm
+++ lib/CatalystX/OAuth2.pm
@@ -158,9 +158,11 @@ The entity representing the client in your schema
 
 =head1 SPONSORSHIP
 
-This module exists due to the wonderful people at Suretec Systems
-Ltd. <http://www.suretecsystems.com/> who sponsored it's development for its
-VoIP division called SureVoIP <http://www.surevoip.co.uk/>
+This module exists due to the wonderful people at Suretec Systems Ltd.
+<http://www.suretecsystems.com/> who sponsored its development for its
+VoIP division called SureVoIP <http://www.surevoip.co.uk/> for use with
+the SureVoIP API - 
+<http://www.surevoip.co.uk/support/wiki/api_documentation>
 
 =cut
 
